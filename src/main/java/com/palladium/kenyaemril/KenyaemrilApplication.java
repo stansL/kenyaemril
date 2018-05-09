ffce19a7db7b708c5d58b028d20172408c65f9bd
Stanslaus <mstans8@gmail.com>
2018-05-09 12:04:00 +0300

    Initial commit

---
+++ src/main/java/com/palladium/kenyaemril/KenyaemrilApplication.java
@@ -0,0 +1,12 @@
+package com.palladium.kenyaemril;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenyaemrilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KenyaemrilApplication.class, args);
+	}
+}
